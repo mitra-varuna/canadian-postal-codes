84dd641f1575e32770c510067f53a59204ce695b
Sushant Srivastava <sushant.srivastav@gmail.com>
2020-10-26 14:08:27 +0530

    Generated Lexer Code
    Signed-off-by: Sushant Srivastava <sushant.srivastav@gmail.com>

---
+++ src/main/gen/in/ssushant/sdk/language/parser/CanadianPostalCodeParserUtil.java
@@ -0,0 +1,6 @@
+package in.ssushant.sdk.language.parser;
+
+import com.intellij.lang.parser.GeneratedParserUtilBase;
+
+public class CanadianPostalCodeParserUtil extends GeneratedParserUtilBase {
+}
